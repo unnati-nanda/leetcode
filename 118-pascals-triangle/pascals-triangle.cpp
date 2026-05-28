2109f1bf4b50a3ea953aea8bd4e9a1b29e020024
Unnati Nanda <unnatinanda10a@gmail.com>
2026-05-28 10:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (62.78%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,13 +5,13 @@ public:
 
         v.push_back({1});
         for (int i = 1; i < numRows; i++) {
-            vector<int> vect;
-            vect.push_back(1);
+            vector<int> vect(i+1,1);
+           vector<int>&prev=v[i-1];
 
             for (int j = 1; j < i; j++) {
-                vect.push_back(v[i - 1][j] + v[i - 1][j - 1]);
+                vect[j]=prev[j]+prev[j-1];
             }
-            vect.push_back(1);
+            
             v.push_back(vect);
         }
 
