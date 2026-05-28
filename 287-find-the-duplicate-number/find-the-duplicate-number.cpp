1dd7b8332d96cc412eb5bc8b5c6de87f36b9435c
Unnati Nanda <unnatinanda10a@gmail.com>
2026-05-28 15:15:30 +0530

    Time: 35 ms (28.22%) | Memory: 65.1 MB (40.77%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int v=0;
+ 
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]){
-                v=nums[i];
+                return nums[i];
             }
         }
-        return v;
+        return -1;
     }
 };
