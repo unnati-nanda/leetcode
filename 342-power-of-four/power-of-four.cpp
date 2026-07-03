94efa7cca3a8b7a090d9c4e119e01082abf2d869
Unnati Nanda <unnatinanda10a@gmail.com>
2026-07-03 13:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (10.79%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n == 0) {
+            return false;
+        }
+        return (n == 1 || (n % 4 == 0 && isPowerOfFour(n / 4)));
+
+        return false;
+    }
+};
